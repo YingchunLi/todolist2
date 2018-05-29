e6156af1fba41d13f25e9855b7d3705e3808d644
Yingchun Li <y.li@eventzero.com>
2018-05-29 23:14:05 +1000

    update test cases

--- server/src/test/java/com/yingchunli/todolist/TodoControllerTest.java
+++ server/src/test/java/com/yingchunli/todolist/TodoControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
@@ -72,13 +73,35 @@ public class TodoControllerTest {
 
 
         mvc.perform(post("/api/todos")
-                .content("{\"name\": \"milk\",\"description\":\"remember the milk\", \"dueDate\":\"2018-12-31T00:00:00.000Z\", \"status\":\"Pendng\"}")
+                .content("{\"name\": \"milk\",\"description\":\"remember the milk\", \"dueDate\":\"2018-12-31T00:00:00.000Z\", \"status\":\"Pending\"}")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(addedTodo.getName())))
                 .andExpect(jsonPath("$.description", is(addedTodo.getDescription())))
+                .andExpect(jsonPath("$.dueDate", containsString("2018-12-31")))
                 .andExpect(jsonPath("$.status", is(addedTodo.getStatus().toString())));
 
         then(todoService).should().addTodo(any(Todo.class));
+    }
+
+    @Test
+    public void whenAddInvalidTodo_thenReturnError() throws Exception {
+        Todo addedTodo = new Todo(
+                "milk",
+                "remember the milk - changed",
+                Date.from(Instant.parse("2018-12-31T00:00:00.000Z")),
+                Todo.Status.Pending
+        );
+        addedTodo.setId(UUID.randomUUID());
+
+        given(todoService.addTodo(any(Todo.class))).willReturn(addedTodo);
+
+
+        mvc.perform(post("/api/todos")
+                .content("{\"description\":\"remember the milk\", \"dueDate\":\"2018-12-31T00:00:00.000Z\", \"status\":\"Pendng\"}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+
+        then(todoService).shouldHaveZeroInteractions();
     }
 }
--- server/src/test/java/com/yingchunli/todolist/TodolistApplicationTests.java
+++ server/src/test/java/com/yingchunli/todolist/TodolistApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,8 +14,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.Instant;
 import java.util.Date;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,6 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(
+		locations = "classpath:application-integrationtest.properties")
 public class TodolistApplicationTests {
 
 	@Autowired
@@ -55,6 +59,26 @@ public class TodolistApplicationTests {
 				.andExpect(jsonPath("$[0].name", is("milk")))
 				.andExpect(jsonPath("$[0].description", is("remember the milk")))
 				.andExpect(jsonPath("$[0].status", is("Pending")));
+	}
+
+	@Test
+	public void whenAddInvalidTodo_thenReturnError() throws Exception {
+		mvc.perform(post("/api/todos")
+				.content("{\"description\":\"remember the milk\", \"dueDate\":\"2018-12-31T00:00:00.000Z\", \"status\":\"Pending\"}")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is4xxClientError());
+	}
+
+	@Test
+	public void whenAddValidTodo_thenReturnAddedTodoAsJson() throws Exception {
+		mvc.perform(post("/api/todos")
+				.content("{\"name\": \"milk\", \"description\":\"remember the milk\", \"dueDate\":\"2018-12-31T00:00:00.000Z\", \"status\":\"Pending\"}")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value("milk"))
+				.andExpect(jsonPath("$.description").value("remember the milk"))
+				.andExpect(jsonPath("$.dueDate", startsWith("2018-12-31")))
+				.andExpect(jsonPath("$.status").value("Pending"));
 	}
 
 }
