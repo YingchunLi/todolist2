b609c87959c260fc1be703f714a07cf368df3885
Yingchun Li <y.li@eventzero.com>
2018-05-29 21:42:10 +1000

    Static responsive UI mock-up using pure css (grid, flexbox and media query)

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,8 +5,29 @@ import TodoList from './TodoList';
 class App extends Component {
   render() {
     return (
-      <div >
-        <TodoList />
+      <div className="ui">
+        <nav className="appbar">Todo List</nav>
+        <nav className="boardbar">Dashboard</nav>
+        <div className="todolists">
+          <article className="todolist">
+            <header>Pending todos</header>
+            <ul>
+              <li>Milk</li>
+              <li>Grocery</li>
+            </ul>
+            <footer>Add new</footer>
+          </article>
+
+          <article className="todolist">
+            <header>Done todos</header>
+            <ul>
+              <li>Gift for tony</li>
+              <li>A very long todo which probably wrap two lines if not then adding more to the title</li>
+              <li>Another todo item that is done</li>
+            </ul>
+            <footer>list footer</footer>
+          </article>
+        </div>
       </div>
     );
   }
